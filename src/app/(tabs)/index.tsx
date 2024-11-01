88e06b0273da8b65c8f4a771b76542be40cf2424
Éric  António <ericantoniodev@gmail.com>
2024-11-01 20:35:33 +0200

    Starting Project
    This is just a Learning Project. I'm just seeing new thing's and approach's in react native.

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,22 @@
+import { theme } from "@/theme";
+import { Text, View, StyleSheet } from "react-native";
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: theme.colors.black,
+  },
+  text: {
+    fontSize: 22,
+    color: theme.colors.white,
+  },
+});
---
+++ src/theme/index.ts
@@ -0,0 +1,6 @@
+import { colors } from "./colors";
+import { fontFamily } from "./fontFamily";
+export const theme = {
+  colors,
+  fontFamily,
+};
